5266619e515115a7e1ed50e5cb12e81415b35583
Iqbal Apriand <149250275+iqbalapriand@users.noreply.github.com>
2024-09-07 16:08:39 +0700

    Update script.js

--- script.js
+++ script.js
@@ -42,3 +42,35 @@ function typeEffect() {
 window.onload = function() {
     typeEffect();
 };
+
+
+
+
+
+let currentIndex = [0, 0, 0, 0];  // Keep track of the current slide for each slider
+
+function showSlide(sliderId, index) {
+    const sliders = document.getElementById('slider' + sliderId).children;
+    const totalSlides = sliders.length;
+    
+    if (index >= totalSlides) {
+        currentIndex[sliderId - 1] = 0;  // Go back to the first slide if it exceeds total slides
+    } else if (index < 0) {
+        currentIndex[sliderId - 1] = totalSlides - 1;  // Go to the last slide if the index is less than 0
+    } else {
+        currentIndex[sliderId - 1] = index;
+    }
+
+    const newTransform = `translateX(-${currentIndex[sliderId - 1] * 100}%)`;
+    document.getElementById('slider' + sliderId).style.transform = newTransform;
+}
+
+function nextSlide(sliderId) {
+    showSlide(sliderId, currentIndex[sliderId - 1] + 1);
+}
+
+function prevSlide(sliderId) {
+    showSlide(sliderId, currentIndex[sliderId - 1] - 1);
+}
+
+
