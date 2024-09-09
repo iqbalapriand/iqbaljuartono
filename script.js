66218d960778b83ddc390e717f8410b76bbe8de4
Iqbal Apriand <149250275+iqbalapriand@users.noreply.github.com>
2024-09-09 22:35:31 +0700

    Update script.js

--- script.js
+++ script.js
@@ -74,3 +74,9 @@ function prevSlide(sliderId) {
 }
 
 
+
+
+document.getElementById('hamburger').addEventListener('click', function() {
+    const navbar = document.getElementById('navbar');
+    navbar.classList.toggle('active');
+});
